72ddb68ccea3dcd81a77f65ae89cd7bf9572aa25
EkeHanson <ekenehanson@gmail.com>
2024-11-19 12:58:20 +0100

    I just want to pull

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 // src/config.js
 const config = {
-  API_BASE_URL: 'https://jumiaonline-api.onrender.com',  // Replace with your base API URL
-  //API_BASE_URL: 'http://127.0.0.1:9090',  // Replace with your base API URL
+  API_BASE_URL: 'https://jumiaonline-api.onrender.com',
+  //API_BASE_URL: 'http://127.0.0.1:9090', 
   };
-  
-  export default config;
 
-
+export default config;
