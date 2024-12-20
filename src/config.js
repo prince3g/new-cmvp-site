393b152ac7bb3cf03ee0d560bb0dbd15c2e7f862
EkeHanson <ekenehanson@gmail.com>
2024-12-20 10:21:47 +0100

    latest commit

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 const config = {
 
-  API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
-  // API_BASE_URL: 'http://127.0.0.1:9090',
+   // API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
+   API_BASE_URL: 'http://127.0.0.1:9090',
 
   };
 
