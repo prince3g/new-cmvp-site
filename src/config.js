ebea3e90f1e9a17168806e83e72692bcf6141563
EkeHanson <ekenehanson@gmail.com>
2024-12-21 16:32:12 +0100

    .

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
 
-   // API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
+    API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
    //API_BASE_URL: 'http://127.0.0.1:9090',
 
 
